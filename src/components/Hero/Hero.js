e81a0ab2be342149d6de21bbbc966f937c9104d9
dermawansil <dermawansilaen19@gmail.com>
2022-03-30 13:14:58 +0700

    NavBar and Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () =>
+{
+    return (
+        <div className="Hero">
+            <h1>Looking fo music</h1>
+            <p>Start Listening a music</p>
+        </div>
+    )
+}
+
+export default Hero;
